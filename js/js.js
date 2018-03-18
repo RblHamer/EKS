e95b73051f062b16f2562cf37ac83b18478b1364
RblHamer <hamer.rulya@gmail.com>
2018-03-18 15:39:00 +0200

    18

---
+++ js/js.js
@@ -0,0 +1,5 @@
+$('.sl').slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    cssEase: 'ease-in',
+});
---
+++ js/text.js
@@ -0,0 +1,7 @@
+var node = document.querySelector('h2');
+var text = new T(node);
+
+function random(min, max) {
+    return (Math.random() * (max - min)) + min;
+}
+
